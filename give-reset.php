3b2607d9cf95ec4498cf4e4b1cbf320b720d1e68
mehul0810 <mehul.gohil0810@gmail.com>
2018-05-30 21:10:17 +0530

    improve code to reset core settings of give plugin

--- give-reset.php
+++ give-reset.php
@@ -47,9 +47,9 @@ function give_reset_display_admin_menu_callback( $wp_admin_bar ) {
 
 	$wp_admin_bar->add_menu( array(
 		'parent' => 'give-reset-main',
-		'id'     => 'give-reset-refresh-install',
-		'href'   => admin_url( '?give_action=refresh_install' ),
-		'title'  => __( 'Refresh Install', 'give-reset' ),
+		'id'     => 'give-reset-core-settings',
+		'href'   => admin_url( '?give_action=reset_core_settings' ),
+		'title'  => __( 'Core Settings', 'give-reset' ),
 		'meta'   => array(
 			'class' => '',
 		),
@@ -61,11 +61,11 @@ function give_reset_display_admin_menu_callback( $wp_admin_bar ) {
 add_action( 'admin_bar_menu', 'give_reset_display_admin_menu_callback' );
 
 /**
- * Callback to refresh install on clicking the button in admin bar.
+ * Callback to reset core settings on clicking the button in admin bar.
  *
  * @since 1.0.0
  */
-function give_refresh_install_action_callback() {
+function give_reset_core_settings_action_callback() {
 
 	// Backup some required settings.
 	$success_page = give_get_option( 'success_page' );
@@ -83,14 +83,28 @@ function give_refresh_install_action_callback() {
 	give_install();
 
 	// Restore the required Give settings.
-	give_update_option( 'success_page', $success_page );
-	give_update_option( 'failure_page', $failure_page );
-	give_update_option( 'history_page', $history_page );
-	give_update_option( 'base_country', $base_country );
-	give_update_option( 'base_state', $base_state );
+	if ( ! empty( $success_page ) ) {
+		give_update_option( 'success_page', $success_page );
+	}
+
+	if ( ! empty( $failure_page ) ) {
+		give_update_option( 'failure_page', $failure_page );
+	}
+
+	if ( ! empty( $history_page ) ) {
+		give_update_option( 'history_page', $history_page );
+	}
+
+	if ( ! empty( $base_country ) ) {
+		give_update_option( 'base_country', $base_country );
+	}
+
+	if ( ! empty( $base_state ) ) {
+		give_update_option( 'base_state', $base_state );
+	}
 
 	wp_safe_redirect( esc_url_raw( admin_url( 'index.php?page=give-about' ) ) );
 	give_die();
 }
-add_action( 'give_refresh_install', 'give_refresh_install_action_callback' );
+add_action( 'give_reset_core_settings', 'give_reset_core_settings_action_callback' );
 
